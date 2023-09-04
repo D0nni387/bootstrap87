631b77ce4bff7509acc52b6e3f5d1ee56c8a81a4
Richard Wells <richard.wells@clickasnap.com>
2023-09-04 19:34:15 +0100

    feat: clears stuff

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,41 @@
-lightGallery(document.getElementById("lightgallery"));
+/**
+ * const used for the DOM
+ */
+// const buttons = document.getElementsByTagName("button");
+
+const buttons = document.getElementsByClassName("control");
+
+const hands = [
+  {
+    name: "rock",
+    beats: "scissors",
+  },
+  {
+    name: "paper",
+    beat: "rock",
+  },
+  {
+    name: "scissors",
+    beats: "paper",
+  },
+];
+
+/**
+ * Adding event listeners to all the buttons
+ */
+for (let button of buttons) {
+  button.addEventListener("click", function () {
+    const userChoice = button.dataset.type;
+    const cpuMove = compMove();
+
+    console.log(cpuMove);
+  });
+}
+
+/**
+ * Changes the comp move based on a random number and adds the corresponding class
+ */
+function compMove() {
+  let cMoveRandom = Math.floor(Math.random() * 3);
+  return hands[cMoveRandom].name;
+}
